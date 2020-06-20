12a6242bc284de2ecf73a785111e9e130193d601
Kai Rautenberg <mail@kairautenberg.de>
2020-06-20 15:54:08 +0200

    +generation time

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import Damp from "../components/damp";
 import Windfinder from "../components/windfinder";
 import Geomar from "../components/geomar";
 
-function IndexPage() {
+function IndexPage({generatedAt}) {
   return (
     <Layout>
       <h2>Damp</h2>
+      <div className="text-sm italic">{generatedAt}</div>
       <Damp />
       <hr className="m-2 border-black" />
       <Geomar />
@@ -14,6 +15,15 @@ function IndexPage() {
       <Windfinder url='https://www.windfinder.com/report/kiel_leuchtturm' />
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const now = new Date()
+  const generatedAt = `${now.toLocaleDateString()}  ${now.toLocaleTimeString()}`
+  return {
+    props: { generatedAt },
+    unstable_revalidate: 5,
+  }
 }
 
 export default IndexPage;
