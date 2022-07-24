851fba729d3af1efc388173062d16efaf44b4cb7
Kai <mail@kairautenberg.de>
2022-07-24 22:58:38 +0200

    fix: remove jsx components form layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,17 +1,3 @@
-import React, { ReactNode } from "react";
-import NavDrawer from "./navDrawer";
-import { NavContextProvider } from "../context/navContext";
-import NavToggle from "./navToggle";
-
-export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <main className="relative text-white bg-black">
-      <NavContextProvider>
-        <div className="absolute top-0 right-0 z-10 w-24 h-full bg-white opacity-50"></div>
-        <NavDrawer />
-        <NavToggle classList="absolute top-0 right-0 m-2 z-30" />
-        {children}
-      </NavContextProvider>
-    </main>
-  );
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <main className="relative text-white bg-black">{children}</main>;
 }
