82e673c16ecd1b3d093ecbecae13af4b2aa66327
Kai <mail@kairautenberg.de>
2022-07-24 23:01:56 +0200

    fix: replace Layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,8 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <main className="relative text-white bg-black">{children}</main>;
+  return (
+    <div className="layout">
+      <h1>with layout</h1>
+      {children}
+    </div>
+  );
 }
