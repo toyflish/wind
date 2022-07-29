738ecc4250c9e57e7602ece949e546cbb6f8fbaa
Kai Rautenberg <mail@kairautenberg.de>
2022-07-29 22:02:09 +0200

    Fix: text

--- components/Msw.tsx
+++ components/Msw.tsx
@@ -30,7 +30,7 @@ export default function Msw() {
           className="inline-flex items-center px-3 py-2 mt-4 text-sm font-medium leading-4 text-white border border-transparent rounded-md shadow-sm bg-slate-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           onClick={handleLoad}
         >
-          Lade Msw
+          Load magic seaweed
         </button>
       )}
       {show && (
